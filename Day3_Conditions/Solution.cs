02124a4d87b74e6d406dc537f9a83122cb32edfe
Alex Macniven <apmacniven@outlook.com>
2019-02-27 18:59:52 +0000

    Adds Day3_Conditions

---
+++ Day3_Conditions/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Day3_Conditions
+{
+    class Solution
+    {
+
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N % 2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if ((N >= 6) && (N <= 20))
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                Console.Write("Not Weird");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
