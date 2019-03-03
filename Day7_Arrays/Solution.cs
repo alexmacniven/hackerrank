b9d577ff18de10294b758afa9c107f510d4204e7
Alex Macniven <apmacniven@outlook.com>
2019-03-03 16:37:34 +0000

    Adds Day7 solution

---
+++ Day7_Arrays/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string result = "";
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+        for(int i = arr.Length - 1; i >= 0; i--)
+        {
+            result = result + arr[i];
+        }
+        Console.WriteLine(result);
+    }
+}
