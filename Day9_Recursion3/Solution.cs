ff3b6b4ea051d04cea9d378dd868ab4c4a3d98ce
Alex Macniven <apmacniven@outlook.com>
2019-03-05 20:36:51 +0000

    Adds Day9 solution

---
+++ Day9_Recursion3/Solution.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * factorial - the product of an integer and all the integers below it; e.g. factorial four ( 4! ) is equal to 24.
+ * 
+ * This solution solves Day 9 of HackerRanks 30 Days of Code.
+ */
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    static int Factorial(int n)
+    {
+        if(n == 1)
+        {
+            return n;
+        }
+        else
+        {
+            return n * Factorial(n - 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = Factorial(n);
+
+        // textWriter.WriteLine(result);
+        Console.WriteLine(result);
+
+        // textWriter.Flush();
+        // textWriter.Close();
+        Console.ReadLine();
+    }
+}
