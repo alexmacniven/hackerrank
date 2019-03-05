38bab70fab8b611fbc2493154215b196f61e1293
Alex Macniven <apmacniven@outlook.com>
2019-03-05 20:09:12 +0000

    Adds Day8 solution

---
+++ Day8_DictionariesAndMaps/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int tests = Convert.ToInt32(Console.ReadLine());
+
+        Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+        for(int i = 0; i < tests; i++)
+        {
+            string input = Console.ReadLine();
+            string[] elems = input.Split(' ');
+            phoneBook.Add(elems[0], elems[1]);
+        }
+
+        string[] argarr= new string[tests];
+
+        for(int i = 0; i < tests; i++)
+        {
+            string query = Console.ReadLine();
+            argarr[i] = query;
+        }
+
+        foreach (string item in argarr)
+        {
+            try
+            {
+                Console.WriteLine(item + "=" + phoneBook[item]);
+            }
+            catch (KeyNotFoundException)
+            {
+
+                Console.WriteLine("Not found");
+            }
+        }
+    }
+}
