cebe09fefdc61b662133cda8eb70d51953819bcf
Alex Macniven <apmacniven@outlook.com>
2019-03-10 16:26:59 +0000

    Adds Day11 solution

---
+++ Day11_ 2DArrays/README.md
@@ -0,0 +1,33 @@
+﻿# README
+
+## Notes
+The hourglass can be considered as 7 coordinates in the grid
+```
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+``` 
+Considering the first hourglass
+```
+1 1 1
+  1  
+1 1 1
+```
+As being made up of the coordinates
+`(0, 0), (0, 1), (0, 2), (1, 1), (2, 0), (2, 1), (2, 2)`
+
+Or with variables
+```
+x = 0  // x-axis
+y = 0  // y-axis
+
+(y, x), (y, x + 1), (y, x + 2), (y + 1, x + 1), (y + 2, x), (y + 2, x + 1), (y + 2, x + 2)
+```
+
+Now to reference the next hourglass, we increment x by 1 while x is less than the length minus 2
+
+When the above condition is met; move down to the next row. That is y + 1 while y is less than the
+length minus 2
---
+++ Day11_ 2DArrays/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+
+
+    static void Main(string[] args)
+    {
+        int[][] arr = new int[6][];
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        int best_score = -99;
+        int current_score = 0;
+
+        for(int yaxis = 0; yaxis <= arr.Length - 3; yaxis++)
+        {
+            for(int xaxis = 0; xaxis <= arr[yaxis].Length - 3; xaxis++)
+            {
+                current_score = arr[yaxis][xaxis] + arr[yaxis][xaxis + 1] + arr[yaxis][xaxis + 2] + 
+                    arr[yaxis + 1][xaxis + 1] + arr[yaxis + 2][xaxis] + arr[yaxis + 2][xaxis + 1] + 
+                    arr[yaxis + 2][xaxis + 2];
+                if(current_score > best_score)
+                {
+                    best_score = current_score;
+                }
+            }
+        }
+
+        Console.WriteLine(Convert.ToString(best_score));
+        Console.ReadLine();
+    }
+}
