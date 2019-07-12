0c1ba713dd1ce050dd52dda1df1c8f286d9ce279
Alex Macniven <apmacniven@outlook.com>
2019-07-12 14:18:52 +0100

    Day 25 Solution
    This commit adds a solution and documentation for Hackerrank Day 25 - Running Time and Complexity.

---
+++ Day25_RunningTimeComplexity/Day25_RunningTimeComplexity/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static bool PrintPrime(int number)
+    {
+        for(int i = number - 1; i > 1; i--)
+        {
+            if(number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static bool PrintPrimev2(int number)
+    {
+        if(number == 1)
+        {
+            return false;
+        }
+        for (int i = (int)Math.Sqrt(number); i > 1; i--)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void Main(String[] args)
+    {
+        /* Enter your code here. 
+         * Read input from STDIN. 
+         * Print output to STDOUT. 
+         * Your class should be named Solution 
+         */
+        int T = Int32.Parse(Console.ReadLine());
+        int[] numbers = new int[T];
+        for(int i = 0; i < T; i++)
+        {
+            numbers[i] = Int32.Parse(Console.ReadLine());
+        }
+
+        for(int i = 0; i < T; i++)
+        {
+            if(PrintPrimev2(numbers[i]))
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Not prime");
+            }
+        }
+        Console.ReadLine();
+    }
+}
---
+++ Day25_RunningTimeComplexity/README.md
@@ -0,0 +1,18 @@
+﻿# Day 25 - Running Time and Complexity
+
+**Challenge:** https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem
+
+Today's challenge was to design an algorithm for evaluating a number as prime.
+The algorithm also had to have a complexity of `O(√n)`.
+
+Firstly I developed a brute force approach in `PrintPrime`.
+This was a basic attempt at evaluating a number as prime by trying to divide the number `n` by each number less than it excluding 1.
+Generally this worked great for smaller values, but as the values increased the running time become noticibly longer.
+
+After some refreshing on the subject, I learnt that a if the square root of a number is prime than the number itself is prime.
+This allows the optimised method `PrintPrimev2` with a complexity of `O(√n)` 🎉
+
+## What I learnt
+ - Constant, Logarithmic, Linear, Quadriatic complexities
+ - Calculating complexity
+ - C#'s System.Math.Sqrt
