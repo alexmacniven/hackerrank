079d8453cb6ada1c28854cfddc6a0a61065fd236
Alex Macniven <macniven.ap@gmail.com>
2019-07-21 21:42:22 +0100

    Day28 Regex Patterns
    This commit adds a solution for the day 28 challenge.

---
+++ Day28_Regex_Patterns/Day28_Regex_Patterns/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    class RegexCheck
+    {
+        public Regex rgx;
+
+        public RegexCheck()
+        {
+            this.rgx = new Regex(@".+@gmail.com$");
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        RegexCheck regexCheck = new RegexCheck();
+        List<string> results = new List<string>();
+
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        for (int NItr = 0; NItr < N; NItr++)
+        {
+            string[] firstNameEmailID = Console.ReadLine().Split(' ');
+
+            string firstName = firstNameEmailID[0];
+
+            string emailID = firstNameEmailID[1];
+
+            if (regexCheck.rgx.IsMatch(emailID))
+            {
+                results.Add(firstName);
+            }
+        }
+        results.Sort();
+        foreach(string s in results)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Day28_Regex_Patterns/README.md
@@ -0,0 +1,11 @@
+# Day28 Regex Patterns
+
+Today's challenge can be found [here](https://www.hackerrank.com/challenges/30-regex-patterns/problem)
+
+And my solution, written in C# is right [here](./Day28_Regex_Patterns/Program.cs)
+
+## Learnings
+
+ - C#'s System.Text.RegularExpressions
+ - Verbatim string literals
+ - String `replace` and `match` with regex
